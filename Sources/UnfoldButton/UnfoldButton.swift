d2833387ff51de8064eeaaf185dd9171cfdce145
Unpxre <unpxre.tw@gmail.com>
2021-02-05 15:56:55 +0800

    [modify]

--- Sources/UnfoldButton/UnfoldButton.swift
+++ Sources/UnfoldButton/UnfoldButton.swift
@@ -15,18 +15,31 @@ public final class UnfoldButton<Type: ButtonContent>: UIViewController {
 
     public var closeAction: (() -> Void) = {}
 
-    public var select: ((Type) -> Void)?
+    public var selectAction: ((Type) -> Void)?
+
+    public lazy var setUseful: (([Type]) -> Void) = { [self] in
+        allSelection = $0
+        cleanAllButton()
+        loadAllButton()
+    }
 
     // MARK: Private Variable
 
+    private var allSelection: [Type] = []
     private var selected: Type = .init(by: 0)
     private var buttons: [UIButton] = []
+    private lazy var closeAnchor: NSLayoutConstraint = view.widthAnchor.constraint(equalToConstant: defaultSize)
 
     // MARK: Lifecycle
 
     public init() {
+        allSelection = Type.allCases.map { $0 }
         super.init(nibName: nil, bundle: nil)
         view.translatesAutoresizingMaskIntoConstraints = false
+        DispatchQueue.main.async { [self] in
+            NSLayoutConstraint.activate([view.heightAnchor.constraint(equalToConstant: defaultSize), closeAnchor])
+        }
+        loadAllButton()
     }
 
     required init?(coder: NSCoder) {
@@ -37,7 +50,27 @@ public final class UnfoldButton<Type: ButtonContent>: UIViewController {
 
     // MARK: Private Function
 
-    private func loadAllButton() {
+    private func cleanAllButton() {
+        buttons.forEach { $0.removeFromSuperview() }
+        buttons.removeAll()
+    }
 
+    private func loadAllButton() {
+        for selection in allSelection {
+            let button: UIButton = .init()
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setImage(selection.contentImage?.withRenderingMode(.alwaysTemplate), for: .normal)
+            button.tag = selection.contentIndex
+            DispatchQueue.main.async { [self] in
+                view.addSubview(button)
+                button.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+                if let leadingButton = buttons.last {
+                    button.leadingAnchor.constraint(equalTo: leadingButton.trailingAnchor).isActive = true
+                } else {
+                    button.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+                }
+            }
+            buttons.append(button)
+        }
     }
 }
