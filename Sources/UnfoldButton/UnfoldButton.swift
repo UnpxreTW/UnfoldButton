8151e665341fa5fa588a47b2d65a68dd46bc5668
Unpxre <unpxre.tw@gmail.com>
2021-02-05 14:42:39 +0800

    [test]

--- Sources/UnfoldButton/UnfoldButton.swift
+++ Sources/UnfoldButton/UnfoldButton.swift
@@ -1,1 +1,29 @@
+//
+//  UnfoldButton.swift
+//  UnfoldButton
+//
+//  Created by UnpxreTW on 2021/02/05.
+//  Copyright © 2021 UnpxreTW. All rights reserved.
+//
+import UIKit
 
+public final class UnfoldButton: UIViewController {
+
+    // MARK: Public Variable
+
+    // MARK: Private Variable
+
+    // MARK: Lifecycle
+
+    init(test: Int) {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Public Function
+
+    // MARK: Private Function
+}
