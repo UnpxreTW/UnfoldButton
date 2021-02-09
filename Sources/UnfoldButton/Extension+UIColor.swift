190758aa9fbe162d85151d8330eec92aaf22558f
Unpxre <unpxre.tw@gmail.com>
2021-02-09 14:20:25 +0800

    [clean]

---
+++ Sources/UnfoldButton/Extension+UIColor.swift
@@ -0,0 +1,19 @@
+//
+//  Extension+UIColor.swift
+//  UnfoldButton
+//
+//  Created by UnpxreTW on 2021/02/09.
+//  Copyright © 2021 UnpxreTW. All rights reserved.
+//
+import UIKit.UIColor
+
+extension UIColor {
+
+    static var buttonColor: UIColor {
+        if #available(iOS 13.0, *) {
+            return label
+        } else {
+            return white
+        }
+    }
+}
--- Sources/UnfoldButton/UnfoldButton.swift
+++ Sources/UnfoldButton/UnfoldButton.swift
@@ -7,17 +7,6 @@
 //
 import UIKit
 
-private extension UIColor {
-
-    static var buttonColor: UIColor {
-        if #available(iOS 13.0, *) {
-            return label
-        } else {
-            return white
-        }
-    }
-}
-
 public protocol UnfoldButtonDelegate: AnyObject {
 
     func tapped<Type: ButtonContent>(_ selected: Type?)
@@ -29,7 +18,7 @@ public final class UnfoldButton<Type: ButtonContent>: UIViewController {
 
     public weak var delegate: UnfoldButtonDelegate?
 
-    public var buttonSize: CGSize = .init(width: 55, height: 55)
+    public var size: CGSize = .init(width: 55, height: 55)
 
     public lazy var selectAction: ((Type) -> Void)? = { [self] in
         selected = $0
@@ -79,7 +68,7 @@ public final class UnfoldButton<Type: ButtonContent>: UIViewController {
         setOpenConstraint()
         setCloseConstraint()
         DispatchQueue.main.async { [self] in
-            NSLayoutConstraint.activate([view.heightAnchor.constraint(equalToConstant: buttonSize.height)])
+            NSLayoutConstraint.activate([view.heightAnchor.constraint(equalToConstant: size.height)])
             NSLayoutConstraint.activate(closeConstraints)
         }
     }
@@ -131,8 +120,8 @@ public final class UnfoldButton<Type: ButtonContent>: UIViewController {
             DispatchQueue.main.async { [self] in
                 view.addSubview(button)
                 NSLayoutConstraint.activate([
-                    button.widthAnchor.constraint(equalToConstant: buttonSize.width),
-                    button.heightAnchor.constraint(equalToConstant: buttonSize.height),
+                    button.widthAnchor.constraint(equalToConstant: size.width),
+                    button.heightAnchor.constraint(equalToConstant: size.height),
                     button.topAnchor.constraint(equalTo: view.topAnchor)
                 ])
             }
